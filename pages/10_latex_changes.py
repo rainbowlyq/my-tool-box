533507bb91e768df43114e57cc59fa424f4edf36
rainbow <937839429@qq.com>
2024-08-08 09:55:39 +0800

    add remove latex changes

---
+++ pages/10_latex_changes.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : 10_latex_changes.py
+@Time    : 2024/8/5 15:38
+@Author  : lyq
+@Description : 
+"""
+import streamlit as st
+import pyperclip
+
+st.set_page_config(page_title="Latex Changes", page_icon="🗓", layout="wide")
+
+
+def parse_latex_changes(text):
+    from TexSoup import TexSoup
+    soup = TexSoup(text)
+    for add_tag in soup.find_all('added'):
+        text = text.replace(repr(add_tag), add_tag.string)
+    for del_tag in soup.find_all('deleted'):
+        text = text.replace(repr(del_tag), '')
+    for rep_tag in soup.find_all('replaced'):
+        text = text.replace(repr(rep_tag), rep_tag.expr.args[0].string)
+    return text
+
+
+st.title(r"Remove $\LaTeX$ Changes")
+txt = st.text_area("input text")
+if txt:
+    output = parse_latex_changes(txt)
+    st.text_area("output text", output)
+    try:
+        pyperclip.copy(output)
+        st.success("Copied to clipboard")
+    except:
+        st.error("Failed to copy to clipboard. Please copy manually.")
